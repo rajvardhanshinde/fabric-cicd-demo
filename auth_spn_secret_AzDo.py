c7b924f02aa5a41bd9ee25b901cbdda215aaec08
Rajvardhan shinde <shinderajvardhan2051@gmail.com>
2025-08-25 12:46:16 +0530

    Update auth_spn_secret_AzDo.py

--- auth_spn_secret_AzDo.py
+++ auth_spn_secret_AzDo.py
@@ -1,1 +1,25 @@
+from fabric_cicd import FabricWorkspace, publish_all_items, unpublish_all_orphan_items
+import argparse
+
+parser = argparse.ArgumentParser(description='Deploy items to Fabric')
+parser.add_argument('--WorkspaceId', type=str, required=True)
+parser.add_argument('--Environment', type=str, required=True)
+parser.add_argument('--RepositoryDirectory', type=str, required=True)
+parser.add_argument('--ItemsInScope', type=str, required=True)
+args = parser.parse_args()
+
+# Split ItemsInScope into list
+item_type_in_scope = args.ItemsInScope.split(",")
+
+# Define target workspace
+target_workspace = FabricWorkspace(
+    workspace_id=args.WorkspaceId,
+    environment=args.Environment,
+    repository_directory=args.RepositoryDirectory,
+    item_type_in_scope=item_type_in_scope,
+)
+
+# Publish and clean orphan items
+publish_all_items(target_workspace)
+unpublish_all_orphan_items(target_workspace)
 
